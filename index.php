f36be5b0e2788f3060473d17683816b0a79a5197
Connector403 <hafiz-2010@hotmail.co.uk>
2021-04-30 12:11:00 +0100

    load profile modal implementation in progress

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Mobile first web app. Administration page for a company that allows to monitor departments, employees, and location that the employees and departments resides in">
+  <meta name="description"
+    content="Mobile first web app. Administration page for a company that allows to monitor departments, employees, and location that the employees and departments resides in">
   <meta name="author" content="Hafizullah Karim">
   <title>Company Directory</title>
   <!-- Bootstrap core CSS -->
@@ -17,183 +18,227 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body>
   <div class="d-flex" id="wrapper">
-      <!-- Sidebar -->
-      <div class="bg-light border-right" id="sidebar-wrapper">
-        <div class="sidebar-heading">Company Directory </div>
-        <div class="list-group list-group-flush">
-          <a href="#emp-submenu" class="list-group-item list-group-item-action bg-light nav-link collapsed text-truncate" data-toggle="collapse" data-target="#emp-submenu"> <i class="fa fa-user" aria-hidden="true"></i> <span class="nav-label">Employee</span></a>
-            <div class="collapse" id="emp-submenu" aria-expanded="false">
-              <ul class="flex-column pl-2 nav">
-                <li class="nav-item">
-                  <a class="nav-link p-1" href="#">
-                      <i class="fa fa-fw fa-dashboard"></i> Dashboard </a>
-              </li>
-              <li class="nav-item">
-                  <a class="nav-link p-1" href="#">
-                      <i class="fa fa-fw fa-bar-chart"></i> Edit </a>
-              </li>
-              </ul>
+    <!-- Sidebar -->
+    <div class="bg-light border-right" id="sidebar-wrapper">
+      <div class="sidebar-heading">Company Directory </div>
+      <div class="list-group list-group-flush">
+        <a href="#emp-submenu" class="list-group-item list-group-item-action bg-light nav-link collapsed text-truncate"
+          data-toggle="collapse" data-target="#emp-submenu"> <i class="fa fa-user" aria-hidden="true"></i> <span
+            class="nav-label">Employee</span></a>
+        <div class="collapse" id="emp-submenu" aria-expanded="false">
+          <ul class="flex-column pl-2 nav">
+            <li class="nav-item">
+              <a class="nav-link p-1" href="#">
+                <i class="fa fa-fw fa-dashboard"></i> Dashboard </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link p-1" href="#">
+                <i class="fa fa-fw fa-bar-chart"></i> Edit </a>
+            </li>
+          </ul>
+        </div>
+        <a class="list-group-item list-group-item-action bg-ligh nav-link collapsed text-truncate" href="#dep-submenu"
+          data-toggle="collapse" data-target="#dep-submenu"> <i class="fa fa-home"></i> <span
+            class="nav-label">Departments</span> </a>
+        <div class="collapse" id="dep-submenu" aria-expanded="false">
+          <ul class="flex-column pl-2 nav">
+
+            <li class="nav-item">
+              <a class="nav-link p-1" href="#">
+                <i class="fa fa-fw fa-dashboard"></i> Dashboard </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link p-1" href="#">
+                <i class="fa fa-fw fa-bar-chart"></i> Edit </a>
+            </li>
+          </ul>
+        </div>
+
+
+        <a href="#loc-submenu" class="list-group-item list-group-item-action bg-light nav-link collapsed text-truncate"
+          data-toggle="collapse" data-target="#loc-submenu"> <i class="fa fa-globe" aria-hidden="true"></i> <span
+            class="nav-label">Employee</span></a>
+        <div class="collapse" id="loc-submenu" aria-expanded="false">
+          <ul class="flex-column pl-2 nav">
+
+            <li class="nav-item">
+              <a class="nav-link p-1" href="#">
+                <i class="fa fa-fw fa-dashboard"></i> Dashboard </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link p-1" href="#">
+                <i class="fa fa-fw fa-bar-chart"></i> Edit </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- /#sidebar-wrapper -->
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+      <nav class="navbar navbar-expand-lg navbar-light  border-bottom" style="background-color: #e3f2fd;">
+        <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" id="search_box" placeholder="Search Profile"
+              aria-label="Search">
+            <button class="btn btn-secondary my-2 my-sm-0" data-toggle="modal" data-target="#profileModal"
+              id="search_button" value="Search" type="button">Search</button>
+          </form>
+        </div>
+      </nav>
+      <!-- Employee Table -->
+      <div id="empTable" class="table-responsive-xs">
+        <div class="table-wrapper ">
+          <div class="table-title">
+            <div class="row">
+              <div class="col-sm-6">
+                <h2>Manage <b>Employees</b></h2>
+              </div>
+              <div class="col-sm-6">
+                <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i
+                    class="material-icons">&#xE147;</i> <span>Add New Employee</span></a>
+                <a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i
+                    class="material-icons">&#xE15C;</i> <span>Delete</span></a>
+              </div>
             </div>
-          <a class="list-group-item list-group-item-action bg-ligh nav-link collapsed text-truncate" href="#dep-submenu" data-toggle="collapse" data-target="#dep-submenu" > <i class="fa fa-home"></i> <span class="nav-label">Departments</span> </a> 
-          <div class="collapse" id="dep-submenu" aria-expanded="false">
-            <ul class="flex-column pl-2 nav">
-              
-              <li class="nav-item">
-                <a class="nav-link p-1" href="#">
-                    <i class="fa fa-fw fa-dashboard"></i> Dashboard </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link p-1" href="#">
-                    <i class="fa fa-fw fa-bar-chart"></i> Edit </a>
-            </li>
-            </ul>
           </div>
-         
-    
-          <a href="#loc-submenu" class="list-group-item list-group-item-action bg-light nav-link collapsed text-truncate" data-toggle="collapse" data-target="#loc-submenu"> <i  class="fa fa-globe" aria-hidden="true"></i> <span class="nav-label">Employee</span></a>
-          <div class="collapse" id="loc-submenu" aria-expanded="false">
-            <ul class="flex-column pl-2 nav">
-              
-              <li class="nav-item">
-                <a class="nav-link p-1" href="#">
-                    <i class="fa fa-fw fa-dashboard"></i> Dashboard </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link p-1" href="#">
-                    <i class="fa fa-fw fa-bar-chart"></i> Edit </a>
-            </li>
-            </ul>
+          <table class="table table-striped table-hover" id="database">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th class="hideCell">jobTitle</th> <!-- hidden on xs screen small (<576px )-->
+                <th class="hideCell">Email</th>
+                <th class="hideCell">Department</th>
+                <th class="hideCell">Location</th>
+                <th>Profile</th>
+              </tr>
+            </thead>
+            <tbody id="empTbody">
+
+
+            </tbody>
+          </table>
+          <div class="clearfix">
+            <div class="hint-text">Showing <b id="entries"></b> Entries </div>
+
           </div>
         </div>
       </div>
-      <!-- /#sidebar-wrapper -->
-      <!-- Page Content -->
-      <div id="page-content-wrapper">
-        <nav class="navbar navbar-expand-lg navbar-light  border-bottom"  style="background-color: #e3f2fd;">
-          <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-            </ul>
-              <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" id="search_box" placeholder="Search Profile" aria-label="Search">
-                <button class="btn btn-secondary my-2 my-sm-0" data-toggle="modal" data-target="#profileModal" id="search_button" value="Search" type="button">Search</button>
-              </form>
+
+      <!-- Profile Modal -->
+      <div class="modal fade" id="profileModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Employee Profile</h5>
             </div>
-          </nav>
-            <!-- Employee Table -->
-            <!-- <div class="container" id="mainContent">
-              <div class="container-table" id="mainTable">
-                <table class="table">
-                  <thead style="position: sticky; top: 0" class="header">
-                      <tr>
-                        <th class="header dont-show1" scope="col">Last Name </th>
-                        <th class="header dont-show2" scope="col">First Name </th>
-                        <th class="header dont-show3" scope="col">Deparment  </th>
-                        <th class="header dont-show4" scope="col">Location </th>
-                        <th class="header dont-show5" scope="col">Location </th>
-                        <th class="header dont-show6" scope="col">Profile</th>
-                        <th class="header dont-show7" scope="col">
-                          <button type="button" id="" class="btn btn-secondary add-small" data-toggle="modal" data-target="#addProfileModal">+</button>
-                        </th>
-                      </tr>
-                    </thead>
-                    <tbody id="tableData" class="read">
-                      <!-- Data will be implemented with jquery/ajax -->
-                 
-                    <!-- </tbody> -->
-                  <!-- </table> -->
-              <!-- </div> -->
-          <!-- </div> --> 
-          <div class="table-responsive-xs">
-            <div class="table-wrapper ">
-              <div class="table-title">
-                <div class="row">
-                  <div class="col-sm-6">
-                    <h2>Manage <b>Employees</b></h2>
-                  </div>
-                  <div class="col-sm-6">
-                    <a href="#addEmployeeModal" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Add New Employee</span></a>
-                    <a href="#deleteEmployeeModal" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Delete</span></a>						
-                  </div>
+            <div class="body-scroll">
+              <div class="modal-body">
+                <div class="">
+                  <form method="post">
+                    <div id="profile-head" class="emp-profile">
+
+                      ...
+                    </div>
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="profile-head">
+                          <ul class="nav nav-tabs" id="myTab" role="tablist">
+                            <li class="nav-item">
+                              <a class="nav-link active" id="home-tab" data-toggle="tab" href="#profile-body-001"
+                                role="tab" aria-controls="home" aria-selected="true">About</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab"
+                                aria-controls="profile" aria-selected="false">Additional Info</a>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="tab-content profile-tab" id="myTabContent">
+                          <div class="tab-pane fade show active emp-profile" id="profile-body-001" role="tabpanel"
+                            aria-labelledby="home-tab">
+
+                            ...
+                          </div>
+                          <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                            <div class="tab-pane fade show active emp-profile" id="profile-body-002" role="tabpanel"
+                              aria-labelledby="home-tab">
+
+                              ...
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
                 </div>
               </div>
-              <table class="table table-striped table-hover">
-                <thead>
-                  <tr>
-                    <th>
-                      <span class="custom-checkbox">
-                        <input type="checkbox" id="selectAll">
-                        <label for="selectAll"></label>
-                      </span>
-                    </th>
-                    <th >Name</th>
-                    <th class="hidden-xs-up">Email</th>  <!-- hidden on xs screen small (<576px )-->
-                    <th >Department</th>
-                    <th>Location</th>
-                    <th >Actions</th>
-                  </tr>
-                </thead>
-                <tbody id="database">
-                  
-                 
-                </tbody>
-              </table>
-              <div class="clearfix">
-                <div class="hint-text">Showing <b>5</b> out of <b>25</b> entries</div>
-                <ul class="pagination">
-                  <li class="page-item disabled"><a href="#">Previous</a></li>
-                  <li class="page-item"><a href="#" class="page-link">1</a></li>
-                  <li class="page-item"><a href="#" class="page-link">2</a></li>
-                  <li class="page-item active"><a href="#" class="page-link">3</a></li>
-                  <li class="page-item"><a href="#" class="page-link">4</a></li>
-                  <li class="page-item"><a href="#" class="page-link">5</a></li>
-                  <li class="page-item"><a href="#" class="page-link">Next</a></li>
-                </ul>
-              </div>
             </div>
-          </div>        
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+          </div>
         </div>
-        <!-- /#page-content-wrapper -->
       </div>
-      <!-- /#wrapper -->
+
+
+
+
+
+    </div>
+    <!-- /#page-content-wrapper -->
+  </div>
+  <!-- /#wrapper -->
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="js/main.js"></script>
   <!-- Menu Toggle Script -->
-    <script>
-      $(document).ready(function(){
-	// Activate tooltip
-	$('[data-toggle="tooltip"]').tooltip();
-	
-	// Select/Deselect checkboxes
-	var checkbox = $('table tbody input[type="checkbox"]');
-	$("#selectAll").click(function(){
-		if(this.checked){
-			checkbox.each(function(){
-				this.checked = true;                        
-			});
-		} else{
-			checkbox.each(function(){
-				this.checked = false;                        
-			});
-		} 
-	});
-	checkbox.click(function(){
-		if(!this.checked){
-			$("#selectAll").prop("checked", false);
-		}
-	});
-});
-      $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
+  <script>
+    $(document).ready(function () {
+      // Activate tooltip
+      $('[data-toggle="tooltip"]').tooltip();
+
+      // Select/Deselect checkboxes
+      var checkbox = $('table tbody input[type="checkbox"]');
+      $("#selectAll").click(function () {
+        if (this.checked) {
+          checkbox.each(function () {
+            this.checked = true;
+          });
+        } else {
+          checkbox.each(function () {
+            this.checked = false;
+          });
+        }
       });
-    </script>
+      checkbox.click(function () {
+        if (!this.checked) {
+          $("#selectAll").prop("checked", false);
+        }
+      });
+    });
+    $("#menu-toggle").click(function (e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  </script>
 </body>
+
 </html>
--- js/main.js
+++ js/main.js
@@ -1,25 +1,222 @@
-$(document).ready(function(){
-    $.ajax({
+
+
+
+
+// Global Variables
+
+let employeeID;
+
+let profile = {
+    firstName: "",
+    lastName: "",
+    jobTitle: "",
+    email: "",
+    department: "",
+    location: ""
+}
+
+$(document).ready(function() {
+    buildTable();
+})
+
+
+
+function clearTable() {
+
+    $('#database').html(`
+    <tbody>
+        <tr id="tableHeader">
+            <th scope="col" class="hideCell" >ID</th>
+            <th scope="col">Display Name</th>
+            <th scope="col" class="hideCell" id="jobTitleHeader">Job Title</th>
+            <th scope="col" class="hideCell">Email</th>
+            <th scope="col" class="hideCell" id="departmentHeader">Department</th>
+            <th scope="col" class="hideCell" id="locationHeader">Location</th>
        
-        url: "php/getAll.php?",
-        type: "GET",
-        async: true,
-        dataType: "text", // should be returning json but instead returns plain text
-        success: function(result){
-           let obj = JSON.parse(result);
-           console.log(obj);
-            return result;
-        },
-        error: function(result) {
-            // console.log('error');
-           let obj = JSON.stringify(result.responseText);
-           $('#database').html(obj);
-           console.log(obj);
-            // console.log(result.responseText);
-            // console.log(typeof result);
+        </tr>
+    </tbody>
+    `)
+}
+
+function appendEntry(db, i, filterBy) {
+
+    $('#database tbody').append(`
+        <tr data-toggle="modal" data-id="${db[i].id}" data-target="#profileModal" onclick="loadProfile(${JSON.stringify(db[i]).split('"').join("&quot;")})">
+            <th class="hideCell">${db[i].id}</th>
+            <td><b> ${db[i].firstName}</b></td>
+            <td><b>${db[i].lastName}</b> 
+            <td class=${(filterBy == "jobTitle") ? "" : "hideCell"}>${db[i].jobTitle}</td>
+            <td class="hideCell">${db[i].email}</td>
+            <td class=${(filterBy == "department") ? "" : "hideCell"}>${db[i].department}</td>
+            <td class=${(filterBy == "location") ? "" : "hideCell"}>${db[i].location}</td>
+            <td><button type="button" id="${db[i].id}" class="btn=btn-secondary table-btn" data-toggle="modal"
+            data-target="#profileModal"> <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eye-fill" fill="currentColor" xmlns="https://www.w3.org/2000/svg">
+            <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
+            <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
+            </svg></button>
+            <td><button style="visibility: hidden"></button></td>'
+            </td>
+        </tr>
+    `)
+
+}
+function buildTable() {
+
+    $.ajax({
+        type: 'GET',
+        url: 'php/getAll.php', 
+        dataType: 'json',
+        success: function(data) {
+
+            var db = data.data;
+
+            var numberOfEntries = 0;
+
+            console.log(db);
+            for (let i in db) {
+                appendEntry(db, i)
+                numberOfEntries++
+            }
+
+            $('#entries').html(numberOfEntries)
+
         }
-    });
-});
+    })
+}
+function loadProfile(profile) {
+    // $('#profileModal').css("display", "block")
+    $('#profile-head').html(
+      '<div class="row">' +
+        '<div class="col-md-6 col-sm-6 col-sx-6">' +
+            '<div class="profile-head">' +
+                '<h5>' +profile.firstName + ' ' + profile.lastName + '</h5>'+
+                '<h5>' + profile.department+ '</h5> ' +
+            '</div>'+
+        '</div>'+
+      '</div>'
+
+    );
+
+    $('#profile-body-001').html(
+        '<div class="row">' +
+            '<div class="col-md-12 text-right"' +
+                '<form>' +
+                    '<input type="number" id="profileId" name="id" value="' + profile.id + ' style="display:none"> </input>' +
+                    '<button type="button" id="edit" class="btn btn-secondary small" data-dismiss="modal" data-toggle="modal" data-target="#updateProfileModal"> Edit </button> ' +
+                '</form>'+
+            '</div'+
+        '</div>'+ 
+        '<div class="row">'+
+            '<div class="col-md-4"'+
+                '<label> First Name: </label>'+
+            '</div>'+
+            '<div class="col-md-8">'+
+                '<p>'+ profile.firstName+ '</p>'+
+            '</div>'+
+        '</div>'+
+        '<hr>'+
+        '<div class="row">'+
+            '<div class="col-md-4">'+
+                '<label> Last Name: </label>'+
+            '</div>'+
+            '<div class="col-md-8"> '+
+                '<p>'+ profile.lastName + '</p>'+
+            '</div>'+
+        '</div>' +
+        '<hr>' +
+        '<div class="row">' +
+            '<div class="col-md-4">' +
+                '<label>Email:</label>' +
+            '</div>' +
+            '<div class="col-md-8">' +
+                '<p>' + profile.email + '</p>' +
+            '</div>' +
+        '</div>' +
+        '<hr>' +
+        '<div class="row">' +
+            '<div class="col-md-4">' +
+            
+                '<label>Department:</label>' +
+            '</div>' +
+            '<div class="col-md-8">' +
+                '<p>' + profile.department + '</p>' +
+            '</div>' +
+        '</div>' +
+        '<hr>' +
+        '<div class="row">' +
+            '<div class="col-md-4">' +
+                '<label>Location:</label>' +
+            '</div>' +
+            '<div class="col-md-4">' +
+                '<p>' + profile.location +
+            '</div>' +
+        '</div>' +
+        '<div class="row">' +
+            '<div class="col-md-4">' +
+            '</div>' +
+            '<div class="col-md-4">' +
+            '</div>' +
+        '</div>' 
+    );
+
+    $('#profile-body-002').html(
+        '<div class="row">' +
+            '<div class="col-md-12 text-right">' +
+                '<form>' +
+                    '<input type="number" id="profileId" name="id" value= "' + profile.jobTitle+ '" style="display: none"></input>' +
+                    '<button type="button" id="edit"  class="btn btn-secondary small" data-dismiss="modal" data-toggle="modal" data-target="#updateProfileModal">Edit</button>' +
+                '</form>' +
+            '</div>' +
+        '</div>' +
+        '<div class="row">' +
+            '<div class="col-md-4">' +
+                '<label>Job Title:</label>' +
+            '</div>' +
+            '<div class="col-md-8">' +
+                '<p>' + rowArray[i][6] + '</p>' +
+            '</div>' +
+        '</div>' +
+        '<hr>' +
+        '<div class="row">' +
+            '<div class="col-md-4">' +
+                '<label>Website:</label>' +
+            '</div>' +
+            '<div class="col-md-8">' +
+                '<p>' + rowArray[i][7] + '</p>' +
+            '</div>' +
+        '</div>' +
+        '<hr>' +
+        '<div class="row">' +
+            '<div class="col-md-4">' +
+                '<label>Address:</label>' +
+            '</div>' +
+            '<div class="col-md-8">' +
+                '<p>' + rowArray[i][8] + '</p>' +
+            '</div>' +
+        '</div>' +
+        '<hr>' +
+        '<div class="row">' +
+            '<div class="col-md-4">' +
+                '<label>Note:</label>' +
+            '</div>' +
+            '<div class="col-md-4">' +
+                '<p>' + rowArray[i][9] + '</p>' +
+            '</div>' +
+        '</div'
+    )
 
 
+    $('#displayName').children().text(`${profile.firstName}  ${profile.lastName}`)
+    $('#id').text(profile.id)
+    $('#firstName').text(profile.firstName)
+    $('#lastName').text(profile.lastName)
+    $('#jobTitle').text(profile.jobTitle)
+    $('#email').text(profile.email)
+    $('#department').text(profile.department)
+    $('#location').text(profile.location)
 
+    if ($('#editModeToggle').prop('checked') == true) {
+        updateProfile()
+    }
+    
+}
