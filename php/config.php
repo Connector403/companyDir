4f2fb058ec40c5899117709cc31a80a962ca3118
Connector403 <hafiz-2010@hotmail.co.uk>
2021-06-01 16:45:17 +0100

    heroku sql server config

--- php/config.php
+++ php/config.php
@@ -1,10 +1,10 @@
 <?php
 
-	$cd_host = "localhost";
+	$cd_host = "eu-cdbr-west-01.cleardb.com";
 	$cd_port = 3306;
 	$cd_socket = "";
-	$cd_user = "hafiz"; // user name
-	$cd_password = "sElling@348"; // password
+	$cd_user = "b67af652fb7d48"; // user name
+	$cd_password = "b2b7c29d"; // password
 	$cd_dbname = "companydirectory"; // database name
 
 ?>
